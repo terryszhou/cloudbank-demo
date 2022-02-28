184a1e7c73a3e4c273d217447c6098db52b5ea54
terryszhou <terryszhou@gmail.com>
2022-02-28 15:10:26 -0600

    Icons added, download icon button stubbed out

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { Stack, Button, Link, Circle, Tooltip, Flex, Text, Heading, Image, Box, HStack, VStack } from "@chakra-ui/react";
+import { Stack, Button, Link, Circle, Tooltip, Flex, Text, Heading, Image, Box, HStack, VStack, IconButton } from "@chakra-ui/react";
 import * as React from "react";
-import './App.css';
+
+import { SiVisualstudiocode } from "react-icons/si";
 
 export const App = () => {
   const colorArray = [
@@ -99,6 +100,37 @@ export const App = () => {
           marginY={"5%"}
           src={"/screenshot.png"}
           alt={"screenshot"} />
+        <Stack marginY={"4%"} spacing={5} alignItems={"center"}>
+          <Text
+            fontFamily={"var(--chakra-fonts-mono)"}
+            fontSize={{ base: "xl", lg: "2xl"}}
+            fontWeight={"bold"}
+            sx={{
+              background: "-webkit-linear-gradient(#d5ad3f, #b96f2e)",
+              "-webkit-background-clip": "text",
+              "-webkit-text-fill-color": "transparent" }}>
+              Download Color Theme Now!
+          </Text>
+          <Tooltip hasArrow label={"Visual Studio Code"}>
+            <IconButton
+              alt={"Visual Studio Download"}
+              as={"a"}
+              bgColor={"#6dd4b2"}
+              boxShadow={"0 0 5px #6dd4b2"}
+              color={"black"}
+              fontSize={"3xl"}
+              href={"https://www.google.com"}
+              icon={<SiVisualstudiocode />}
+              rounded={"full"}
+              size={"lg"}
+              target={"_blank"}
+              width={"fit-content"}
+              _hover={{
+                bgColor: "white",
+                color: "rgb(58,129,202)"
+              }} />
+          </Tooltip>
+        </Stack>
         <Stack marginY={"4%"} spacing={5}>
           <Text
             fontFamily={"var(--chakra-fonts-mono)"}
